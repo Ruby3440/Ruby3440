33fe5d211c012386c0a9f3827a9c83cb28b7e068
Ruby3440 <gokufreacksaiyan123@gmail.com>
2026-04-18 21:06:41 -0400

    Update sm64ap.h

--- src/sm64ap.h
+++ src/sm64ap.h
@@ -139,6 +139,17 @@ AP_EXTERN_C void SM64AP_SendItem(int);
 AP_EXTERN_C void SM64AP_CheckEnemyDeath(struct Object *);
 AP_EXTERN_C void SM64AP_UpdateRRTrapTimer(struct MarioState *);
 
+AP_EXTERN_C bool gFreezeTrapped;
+AP_EXTERN_C s32 gFreezeTrapTimer;
+AP_EXTERN_C Vec3f gFreezePos;
+AP_EXTERN_C Vec3s gFreezeFaceAngle;
+AP_EXTERN_C s32 gFreezeAction;
+AP_EXTERN_C u32 gFreezeActionArg;
+AP_EXTERN_C u16 gFreezeAnimFrame;
+
+AP_EXTERN_C void SM64AP_BeginFreezeTrap(struct MarioState m);
+AP_EXTERN_C bool SM64AP_UpdateFreezeTrap(struct MarioStatem);
+
 // Print Next Message to Screen
 AP_EXTERN_C void SM64AP_PrintNext();
 
